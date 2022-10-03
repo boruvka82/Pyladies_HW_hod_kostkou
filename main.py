99c99835f99ab5e84336c9f537d7de0e3edaf0cd
boruvka82 <102541983+boruvka82@users.noreply.github.com>
2022-10-03 11:12:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-def hod_kostkou():
-   """bude vracet počet pokusů než hráči padla šestka"""
-
 from random import randrange
 
-
-vitez = 0
-maximum = 0
-for hrac in range(1,5):
+def hod_kostkou(hrac):
+    """bude vracet počet pokusů než hráči padla šestka."""
     
-    #number = int(input("zadej cislo: "))
-    pocet_hodu_hrac = 0  
+    pocet_hodu_hrac = 0     
+    
     while True:
-         
         hod = randrange(1,7)
-        pocet_hodu_hrac = pocet_hodu_hrac + 1 
-        print("hrac",hrac, "hodil", hod) 
+        pocet_hodu_hrac = pocet_hodu_hrac + 1   
+        print("hrac", hrac, "hodil", hod) 
         if hod==6:
-            print() 
-            break
-            
-    if pocet_hodu_hrac > maximum:
-        maximum = pocet_hodu_hrac
+            print("celkem", pocet_hodu_hrac)
+            print()
+            break  
+    return pocet_hodu_hrac
+
+maximum = 0
+
+for hrac in range(1,5):
+    celkem_hodu = hod_kostkou(hrac)
+    if maximum < celkem_hodu:
+        maximum = celkem_hodu
         vitez = hrac
-print("vitez hrac",vitez,"maximum",maximum)    
+
+print("vitez je hrac",vitez,"s maximem hodu:",maximum) 
